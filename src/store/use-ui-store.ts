308e90e0c32e78843cf2cfb007db307f813ab79e
globizhub-anupam-saha <anupam.saha@globizhub.com>
2026-07-10 11:26:34 +0530

    feat: add isMobileSidebarOpen state and setter to UI store

--- src/store/use-ui-store.ts
+++ src/store/use-ui-store.ts
@@ -3,12 +3,14 @@ import { create } from 'zustand';
 interface UiStore {
   isCommandPaletteOpen: boolean;
   isShortcutModalOpen: boolean;
+  isMobileSidebarOpen: boolean;
   activeJobDetailsId: string | null;
   activeNotificationReplayJobId: string | null;
   theme: 'dark' | 'light';
 
   setCommandPaletteOpen: (open: boolean) => void;
   setShortcutModalOpen: (open: boolean) => void;
+  setMobileSidebarOpen: (open: boolean) => void;
   setActiveJobDetailsId: (id: string | null) => void;
   setActiveNotificationReplayJobId: (id: string | null) => void;
   setTheme: (theme: 'dark' | 'light') => void;
@@ -18,12 +20,14 @@ interface UiStore {
 export const useUiStore = create<UiStore>((set) => ({
   isCommandPaletteOpen: false,
   isShortcutModalOpen: false,
+  isMobileSidebarOpen: false,
   activeJobDetailsId: null,
   activeNotificationReplayJobId: null,
   theme: 'dark',
 
   setCommandPaletteOpen: (isCommandPaletteOpen) => set({ isCommandPaletteOpen }),
   setShortcutModalOpen: (isShortcutModalOpen) => set({ isShortcutModalOpen }),
+  setMobileSidebarOpen: (isMobileSidebarOpen) => set({ isMobileSidebarOpen }),
   setActiveJobDetailsId: (activeJobDetailsId) => set({ activeJobDetailsId }),
   setActiveNotificationReplayJobId: (activeNotificationReplayJobId) => set({ activeNotificationReplayJobId }),
   setTheme: (theme) => set({ theme }),
